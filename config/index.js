c922d791d95a37f1f68abb86fd3c68b526fd3b2c
Jorden Lowe <supermitsuba@gmail.com>
2015-10-17 01:06:19 -0400

    cleaned up the markdown

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 process.env.EMAILADDRESS = "fbombcode@gmail.com";
 process.env.EMAILSERVICE = "Gmail";
-
+process.env.PORT = 8088;
 process.env.NODE_ENV = "production"; //development or production
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var path = require('path');
 require('./config');
 require('./.config');
 var BlogProvider = require('./services/BlogProvider').BlogProvider;
-var BundleUp = require('bundle-up2');    
+var BundleUp = require('bundle-up2');
 var express = require('express');
+var bodyParser = require('body-parser');
 var emailer = require('nodemailer');
 var cache = require('memory-cache');
 var app = express();
@@ -12,14 +13,13 @@ module.exports = app;
 function main() {
   var http = require('http');
 
-  app.configure('production', function () {
-    // ... ... ...
-  });
+  if(app.get('env') === 'production') {
 
-  app.configure('development', function () {
+  }
+
+  if(app.get('env') === 'development') {
     process.env.DEBUGLOGGING = true;
-    process.env.PORT = 8081;
-  });
+  }
 
   var connectionString = JSON.parse(process.env.MYSQLCONNSTR_TableConnection);
   var blogProvider = new BlogProvider(connectionString.ac, connectionString.akey, cache);
@@ -32,21 +32,26 @@ function main() {
   });
 
   // Configure the application.
-  app.configure(function () {
-    app.set('view engine', 'ejs');
-    app.set('view options', { layout: false });
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.bodyParser());
-    app.use(require('etagify')());
-    BundleUp(app, path.join(__dirname, 'assets.js'), {
-      staticRoot: path.join(__dirname, 'public'),
-      staticUrlRoot: '/',
-      bundle: !process.env.DEBUGLOGGING,
-      minifyCss: !process.env.DEBUGLOGGING,
-      minifyJs: !process.env.DEBUGLOGGING
-    });
+  // app.configure(function () {
+  //
+  // });
+  app.set('view engine', 'ejs');
+  app.set('view options', { layout: false });
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.use(require('etagify')());
+  BundleUp(app, path.join(__dirname, 'assets.js'), {
+    staticRoot: path.join(__dirname, 'public'),
+    staticUrlRoot: '/',
+    bundle: !process.env.DEBUGLOGGING,
+    minifyCss: !process.env.DEBUGLOGGING,
+    minifyJs: !process.env.DEBUGLOGGING
   });
-    
 
   var server = http.createServer(app);
 
